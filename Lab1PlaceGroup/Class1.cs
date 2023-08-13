b5fbafb2df4c59cefe5de55d515decc71aa4ccf9
Arthur Emig <arthur.erich.emig@gmail.com>
2023-08-13 23:36:55 +0200

    picked ref

--- Lab1PlaceGroup/Class1.cs
+++ Lab1PlaceGroup/Class1.cs
@@ -43,3 +43,17 @@ namespace Lab1PlaceGroup
         }
     }
 }
+
+// Filter to constrain picking to model groups. Only model groups
+// are highlighted and can be selected when cursor is hovering.
+public class GroupPickFilter : ISelectionFilter
+{
+    public bool AllowElement(Element e)
+    {
+        return (e.Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_IOSModelGroups));
+    }
+    public bool AllowReference(Reference r, XYZ p)
+    {
+        return false;
+    }
+}
